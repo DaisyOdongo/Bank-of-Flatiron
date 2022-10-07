cdf626fe29876fdd13264f2ac43a05e1c8c3e33b
+ <+>
2022-10-07 22:24:42 +0300

    code challenge completed

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -2,19 +2,14 @@ import React from "react";
 import Transaction from "./Transaction";
 
 function TransactionsList({transcations, handleDeleteTransactions, handleSort}) {
-  const {id}= transcations    
-  const handleDeletions = async (id) =>{
-        console.log(id);
-        try{
-          const response = await fetch("http://localhost:8001/transactions/" + id, {
-            method: "DELETE",
-          });
-          
-          handleDeleteTransactions(id);
-        }catch (error){
-          console.log(error);
-        }
-      };
+
+  function handleDeletions(id) {
+    fetch(`http://localhost:8001/transactions/${id}`, {
+      method: "DELETE",
+    })
+    handleDeleteTransactions(id)
+  }
+
   return (
     <table className="ui celled striped padded table">
       <tbody>
